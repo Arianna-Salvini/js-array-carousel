6900260c97896692051193f37bc68060cc056054
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-23 16:55:12 +0100

    Add array slides Img for collect images Add const and let of necessary element

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 console.log("Hello world");
+
+// creo arrey di img
+const slidesImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
+console.log(slidesImg);
+
+// Creo costanti
+let activeImg = 0
+const viewElement = document.querySelector('.slides')
+console.log(viewElement);
+console.log(activeImg)
+
+
+
