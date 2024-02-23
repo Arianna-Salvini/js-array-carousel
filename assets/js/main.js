4f1819195ea037ac6cc94844cc6da9154e04572e
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-23 17:17:11 +0100

    Add dinamically img from js and delet the static images in hTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,26 @@
 console.log("Hello world");
 
 // Creat arrey for img
-const slidesImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
+const slidesImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
 console.log(slidesImg);
 
 //cost/Let
-let activeImg = 0
-const viewElement = document.querySelector('.slides')
+let activeImg = 
+const viewElement = document.querySelector('.slides');
 console.log(viewElement);
-console.log(activeImg)
+console.log(activeImg);;
 
 // Start for loop
 
 for (let i = 0; i < slidesImg.length; i++) {
     const singelImage = slidesImg[i];
-    console.log(singelImage);
-     let imgMarkup = `<img class=" ${ i === activeImg ? 'active' : ''  }" src="./assets/img/${singelImage}" alt="">`
-     console.log(imgMarkup);
-}
+    let imgMarkup = `<img class=" ${i === activeImg ? 'active' : ''}" src="./assets/img/${singelImage}" alt="">`;
+    console.log(imgMarkup);
+    viewElement.insertAdjacentHTML("afterbegin", imgMarkup);
+
+};
+
+
+
 
 
