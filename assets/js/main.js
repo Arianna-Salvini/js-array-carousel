adaace6334c64b7a9dd4340da2e6b815852d6b84
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-23 17:12:35 +0100

    Add for loop

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,22 @@
 console.log("Hello world");
 
-// creo arrey di img
+// Creat arrey for img
 const slidesImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"]
 console.log(slidesImg);
 
-// Creo costanti
+//cost/Let
 let activeImg = 0
 const viewElement = document.querySelector('.slides')
 console.log(viewElement);
 console.log(activeImg)
 
+// Start for loop
+
+for (let i = 0; i < slidesImg.length; i++) {
+    const singelImage = slidesImg[i];
+    console.log(singelImage);
+     let imgMarkup = `<img class=" ${ i === activeImg ? 'active' : ''  }" src="./assets/img/${singelImage}" alt="">`
+     console.log(imgMarkup);
+}
 
 
