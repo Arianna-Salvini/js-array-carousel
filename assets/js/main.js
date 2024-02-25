17716d67afa29cfd62a9a2a6378c707b0dbb8c35
“Arianna-Salvini” <arianna.salvini@gmail.com>
2024-02-25 20:06:49 +0100

    Fix infinit loop for next button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,9 @@ console.log("Hello world");
 
 // Creat arrey for img
 const slidesImg = ["01.webp", "02.webp", "03.webp", "04.webp", "05.webp"];
-// console.log(slidesImg);
-
 //cost/Let
 let activeImg = 0
+
 const viewElement = document.querySelector('.slides');
 // console.log(viewElement);
 // console.log(activeImg);
@@ -13,49 +12,54 @@ const next = document.getElementById("next");
 const prev = document.getElementById("prev");
 
 // Start for loop
-
-for (let i = 0; i < slidesImg.length; i++) {
+for (let i = 0; i < slidesImg.length -1; i++) {
     const singelImage = slidesImg[i];
     let imgMarkup = `<img class=" ${i === activeImg ? 'active' : ''}" src="./assets/img/${singelImage}" alt="">`;
     // console.log(imgMarkup);
     viewElement.insertAdjacentHTML("beforeend", imgMarkup);
-
 };
-
+// 
 // On click Add event listener to button prev and next
-// console.log(next, prev);
-
-next.addEventListener('click', function(){
+//Next//
+next.addEventListener('click', function () {
     // console.log('next');
-
+  
     activeImg++
+    let maxIndexLenth = (slidesImg.length -1)
+
+    if (activeImg >= maxIndexLenth){
+        activeImg = 0;
+    }
+
     const currentImg = document.querySelector('img.active')
     // console.log(currentImg);
 
     currentImg.classList.remove('active')
     // console.log(currentImg);
 
-    const allImg = document.querySelectorAll(".slides img")
-    // console.log(allImg);
- 
+    const allImg = document.querySelectorAll(".slides>img")
     allImg[activeImg].classList.add('active')
- 
 
 })
-
-prev.addEventListener('click', function(){
+//Prev//
+prev.addEventListener('click', function () {
     // console.log('prev');
 
     activeImg--
+    let maxIndexLenth = (slidesImg.length -1)
+
+    if (activeImg < 0) {
+        activeImg == maxIndexLenth}
+
     const currentImg = document.querySelector('img.active')
     // console.log(currentImg);
 
     currentImg.classList.remove('active')
     // console.log(currentImg);
 
-    const allImg = document.querySelectorAll(".slides img")
+    const allImg = document.querySelectorAll(".slides>img")
     // console.log(allImg);
- 
+
     allImg[activeImg].classList.add('active')
 })
 
